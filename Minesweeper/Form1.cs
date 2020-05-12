924876258559dc6940a8a917ec8940d29fe49261
InsarL <64440932+InsarL@users.noreply.github.com>
2020-05-12 22:00:08 +0300

    Подогнаны размеры поля и нарисована сетка.

--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -12,9 +12,26 @@ namespace Minesweeper
 {
     public partial class Form1 : Form
     {
+        int cellSize;
+        int gameFieldSize;
+
         public Form1()
         {
             InitializeComponent();
+
+            cellSize = 25;
+            gameFieldSize = 9;
         }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i <= gameFieldSize ; i++)
+            {
+                e.Graphics.DrawLine(Pens.Black, 0, i * cellSize, cellSize * gameFieldSize, i*cellSize);
+                e.Graphics.DrawLine(Pens.Black, i * cellSize, 0, i * cellSize, cellSize * gameFieldSize);
+            }
+        }
+
+        
     }
 }
