ed44075e98e1add881741b55851c1349d800d87c
InsarL <64440932+InsarL@users.noreply.github.com>
2020-05-13 18:17:47 +0300

    Создан массив, отображающий наше игровое поле. Расставлены рандомно мины (-1) а также указано число мин вокруг в клетке.

--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -14,6 +14,10 @@ namespace Minesweeper
     {
         int cellSize;
         int gameFieldSize;
+        int bombCount;
+        int minesAround;
+        int[,] fieldArray;
+        Random random;
 
         public Form1()
         {
@@ -21,17 +25,67 @@ namespace Minesweeper
 
             cellSize = 25;
             gameFieldSize = 9;
+            bombCount = 0;
+            minesAround = 0;
+            fieldArray = new int[gameFieldSize, gameFieldSize];
+            random = new Random();
+
+
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            for (int i = 0; i <= gameFieldSize ; i++)
+            for (int i = 0; i <= gameFieldSize; i++)
             {
-                e.Graphics.DrawLine(Pens.Black, 0, i * cellSize, cellSize * gameFieldSize, i*cellSize);
+                e.Graphics.DrawLine(Pens.Black, 0, i * cellSize, cellSize * gameFieldSize, i * cellSize);
                 e.Graphics.DrawLine(Pens.Black, i * cellSize, 0, i * cellSize, cellSize * gameFieldSize);
+            }
+
+            while (bombCount <= 10)
+            {
+                int x = random.Next(0, 9);
+                int y = random.Next(0, 9);
+
+                if (fieldArray[x, y] == 0)
+                    fieldArray[x, y] = -1;
+                bombCount++;
+            }
+
+            for (int i = 0; i < gameFieldSize; i++)
+            {
+                for (int j = 0; j < gameFieldSize; j++)
+                {
+                    if (!Mine(i, j))
+                    {
+                        if (Mine(i++, j))
+                            minesAround++;
+                        if (Mine(i--, j))
+                            minesAround++;
+                        if (Mine(i, j++))
+                            minesAround++;
+                        if (Mine(i, j--))
+                            minesAround++;
+                        if (Mine(i--, j++))
+                            minesAround++;
+                        if (Mine(i--, j--))
+                            minesAround++;
+                        if (Mine(i++, j++))
+                            minesAround++;
+                        if (Mine(i++, j--))
+                            minesAround++;
+                    }
+                    fieldArray[i, j] = minesAround;
+                }
             }
         }
 
-        
+        bool Mine(int i, int j)
+        {
+            return (i < gameFieldSize && j < gameFieldSize && i >= 0 && j >= 0 && fieldArray[i, j] == -1);
+        }
+
+ 
+      
+            
     }
 }
